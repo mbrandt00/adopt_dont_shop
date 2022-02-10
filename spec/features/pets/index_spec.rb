96428cce9840a17b80fb3d7e4066da371458e82d
Michael Brandt <msbrandt00@gmail.com>
2022-02-10 16:51:38 -0700

    Test: Start a new application for a pet

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,22 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+  it 'has a link to start an application for a new pet' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+    visit "/pets"
+    click_link "Start an Application"
+    expect(current_path).to eq("/pets/application/new")
+    fill_in "name", with: 'John Smith'
+    fill_in "street_address", with: '100 Main Street'
+    fill_in "city", with: 'Portland'
+    fill_in "state", with: 'Oregon'
+    fill_in "zipcode", with: 97035
+    click_button "Submit Application"
+    # expect(current_path). to eq("/pets/application/1")
+    expect(page).to have_content('John Smith')
+    expect(page).to have_content('Zipcode')
+  end
 end
