6790eadeb069427bb3e1d1a3ea5c381d7c9de02c
Michael Brandt <msbrandt00@gmail.com>
2022-02-10 22:19:46 -0700

    Change schema for many to many associations

--- db/migrate/20220210232702_add_description_to_pet_application.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToPetApplication < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pet_applications, :description, :string
-  end
-end
--- db/migrate/20220211013121_create_applications.rb
+++ db/migrate/20220211013121_create_applications.rb
@@ -1,12 +1,15 @@
-class CreatePetApplication < ActiveRecord::Migration[5.2]
+class CreateApplications < ActiveRecord::Migration[5.2]
   def change
-    create_table :pet_applications do |t|
+    create_table :applications do |t|
       t.string :name
       t.string :street_address
       t.string :city
       t.string :state
       t.integer :zipcode
+      t.string :description
       t.string :application_status
+      t.string :desired_dogs, array: true, default: []
+
       t.timestamps
     end
   end
