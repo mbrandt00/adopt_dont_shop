2aee2941f5ddf3a4f9633e731d3bc93b5054aa3b
Michael Brandt <msbrandt00@gmail.com>
2022-02-11 12:28:24 -0700

    Create method with error feedback

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,17 +1,24 @@
 class PetApplicationsController < ApplicationController
+
+  def index
+    @petapplications = PetApplication.all
+  end
   def new
-    @dogs = Pet.all
   end
 
   def create
-
-    params[:desired_dogs] = params[:desired_dogs].reject(&:blank?) #get rid of empty string from checkmark
+    params[:desired_dogs] = params[:desired_dogs].reject(&:blank?).map(&:to_i) #get rid of empty string from checkmark
     pets = Pet.where(id: params[:desired_dogs])
-    application = Application.create(application_params)
+    application = Application.new(application_params)
+    if application.save && pets.present?
     pets.each do |c|
       PetApplication.create(application: application, pet_id: c.id)
     end
-    redirect_to "/pets/application/#{PetApplication.last.id}"
+    redirect_to "/pets/applications/#{PetApplication.last.id}"
+    else
+      flash[:notice] = "Missing Input!"
+      render :new
+    end
   end
 
   def show
@@ -20,6 +27,6 @@ class PetApplicationsController < ApplicationController
 
   private
     def application_params
-      params.permit(:name, :street_address, :city, :state, :zipcode, :description, :desired_dogs)
+      params.permit(:name, :street_address, :city, :state, :zipcode, :description)
     end
 end
