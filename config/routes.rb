299b6cb89d1c0665cf77249c7e3bb0612167414f
Michael Brandt <msbrandt00@gmail.com>
2022-02-12 18:32:38 -0700

    modify routes for admin index

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   patch '/pets/:id', to: 'pets#update'
   delete '/pets/:id', to: 'pets#destroy'
 
+  get '/admin/shelters', to: 'admin#index'
+
   get '/veterinary_offices', to: 'veterinary_offices#index'
   get '/veterinary_offices/new', to: 'veterinary_offices#new'
   get '/veterinary_offices/:id', to: 'veterinary_offices#show'
