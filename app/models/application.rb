c0c958b9c342936eec7a44fe7a84f1d74afe099f
Michael Brandt <msbrandt00@gmail.com>
2022-02-13 10:30:49 -0700

    Feature: approve an application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,11 +1,11 @@
 class Application < ApplicationRecord
-  has_many :pet_applications
+  has_many :pet_applications, dependent: :delete_all
   has_many :pets, through: :pet_applications
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
   validates :zipcode, presence: true
-  enum status: {"In Progress": 0, "Pending": 1, "Accepted": 2, "Rejected": 3}
+  enum status: {"In Progress": 0, "Pending": 1, "Approved": 2, "Rejected": 3}
 
   def adopt(pet)
     self.pets << pet
